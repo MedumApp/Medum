f58bdb8b8944e7dfae7a238bd82b79bc7a681725
MedumApp <medumcorp@gmail.com>
2017-03-26 15:34:44 -0700

    Version 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,18 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+ 
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBLCQIxuJkVuxs6xUC4lWDKwGrRKQ0AmWo",
+    authDomain: "medum-490a6.firebaseapp.com",
+    databaseURL: "https://medum-490a6.firebaseio.com/",
+    storageBucket: "medum-490a6.appspot.com",
+    messagingSenderId: "63225974035"
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,8 @@ import { TabsPage } from '../pages/tabs/tabs';
       TabsPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,133 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
 
+import {AngularFire, FirebaseListObservable} from 'angularfire2';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  houses: FirebaseListObservable<any>;
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFire, public actionSheetCtrl: ActionSheetController) {
+    this.houses=af.database.list('/houses');
   }
+
+  addHouse(){
+  let prompt = this.alertCtrl.create({
+    title: 'Nuevo anuncio',
+    message: "Ingresa las descripciones de tu anuncio",
+    inputs: [
+      {
+        name: 'title',
+        placeholder: 'Titulo'
+      },
+      {
+      	name: 'description',
+      	placeholder: 'Descripcion'
+      },
+      {
+      	name: 'price',
+      	placeholder: 'Precio'
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Guardar',
+        handler: data => {
+          this.houses.push({
+            title: data.title,
+            description: data.description,
+            price:data.price
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+
+showOptions(houseId, houseTitle,houseDescription,housePrice) {
+  let actionSheet = this.actionSheetCtrl.create({
+    title: '¿Que quieres hacer?',
+    buttons: [
+      {
+        text: 'Borrar casa',
+        role: 'destructive',
+        handler: () => {
+          this.removeHouse(houseId);
+        }
+      },{
+        text: 'Editar',
+        handler: () => {
+          this.updateHouse(houseId, houseTitle,houseDescription,housePrice);
+        }
+      },{
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }
+    ]
+  });
+  actionSheet.present();
+}
+
+removeHouse(houseId: string){
+  this.houses.remove(houseId);
+}
+
+updateHouse(houseId, houseTitle,houseDescription,housePrice){
+  let prompt = this.alertCtrl.create({
+    title: 'Editar Anuncio',
+    message: "Edita",
+    inputs: [
+      {
+        name: 'title',
+        placeholder: 'Title',
+        value: houseTitle
+      },
+      {
+      	name: 'description',
+      	placeholder: 'Descripcion',
+      	value:houseDescription
+      },
+      {
+      	name: 'price',
+      	placeholder: 'Precio',
+      	value:housePrice
+      }
+    ],
+    buttons: [
+      {
+        text: 'Cancelar',
+        handler: data => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Guardar',
+        handler: data => {
+          this.houses.update(houseId, {
+            title: data.title,
+            description: data.description,
+            price:data.price
+          });
+        }
+      }
+    ]
+  });
+  prompt.present();
+}
+
+
 
 }
